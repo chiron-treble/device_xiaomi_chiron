91ac3d9f712f6e6ef8ac8508fa060b744585a4cf
andr68rus <ashtmb@gmail.com>
2018-03-15 22:27:38 +0300

    rebrand to lineage

--- lineage.mk
+++ lineage.mk
@@ -1,13 +1,15 @@
-$(call inherit-product, device/xiaomi/chiron/full_chiron.mk)
+$(call inherit-product, device/xiaomi/sagit/full_chiron.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := rr_chiron
+PRODUCT_NAME := lineage_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI Mix 2
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Google client ID.
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
